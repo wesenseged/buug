9789cf31b1ce879180a03d1e1a2d1e5ae43aa1db
wesenseged <wesenseged@skiff.com>
2024-12-30 09:40:49 +0300

    Production commit

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
-// import { cors } from 'hono/cors';
+import { cors } from "hono/cors";
 import { logger } from "hono/logger";
-import { serveStatic } from "hono/bun";
+// import { serveStatic } from "hono/bun";
 import authRoute from "./route/apiRoute";
 import { taskRoute } from "./route/taskRoute";
 import projectRoute from "./route/projectRoute";
@@ -12,11 +12,13 @@ const app = new Hono();
 
 app.use("*", logger());
 
-// app.use(cors({
-//   origin: 'https://buug-client.onrender.com/', // Replace with your frontend URL
-//   allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowHeaders: ['Content-Type', 'Authorization']
-// }));
+app.use(
+  cors({
+    origin: "https://https://buug-client.onrender.com/", // Replace with your frontend URL
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+    allowHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // app.get("/favicon.ico", (c) => {
 //   return c.body(null, 204);
@@ -34,8 +36,8 @@ const routes = app
   .route("/", noteRoute)
   .route("/", chartRoute);
 //
-app.get("*", serveStatic({ root: "./client/dist" }));
-app.get("*", serveStatic({ path: "./client/dist/index.html" }));
+// app.get("*", serveStatic({ root: "./client/dist" }));
+// app.get("*", serveStatic({ path: "./client/dist/index.html" }));
 //
 export type AppType = typeof routes;
 export default app;
