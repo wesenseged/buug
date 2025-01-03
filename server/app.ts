55e76b9f45880055a8365b3cbc5e350121e4ddf3
wesenseged <wesenseged@skiff.com>
2025-01-03 09:09:30 +0300

    Production commit

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { cors } from "hono/cors";
+// import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 // import { serveStatic } from "hono/bun";
 import authRoute from "./route/apiRoute";
@@ -12,25 +12,33 @@ const app = new Hono();
 
 app.use("*", logger());
 
-app.use(
-  "*",
-  cors({
-    origin: "https://buug-client.onrender.com",
-    allowHeaders: ["Content-Type", "Authorization"],
-    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    exposeHeaders: ["Content-Length"],
-    credentials: true, // Allow credentials to be included
-  })
-);
+// app.use(
+//   "*",
+//   cors({
+//     origin: "https://buug-client.onrender.com",
+//     allowHeaders: ["Content-Type", "Authorization"],
+//     allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     exposeHeaders: ["Content-Length"],
+//     credentials: true, // Allow credentials to be included
+//   })
+// );
+
+app.use("*",async(c, next) => {
+  c.header('Access-Control-Allow-Credentials', "true");
+  c.header('Access-Control-Allow-Origin',"https://buug-client.onrender.com");
+  c.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  c.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
 
 // Handle preflight (OPTIONS) requests
-app.options("*", async (c) => {
-  c.header("Access-Control-Allow-Origin", "https://buug-client.onrender.com");
-  c.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  c.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  c.header("Access-Control-Allow-Credentials", "true");
-  c.status(204);
-});
+// app.options("*", async (c) => {
+//   c.header("Access-Control-Allow-Origin", "https://buug-client.onrender.com");
+//   c.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   c.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   c.header("Access-Control-Allow-Credentials", "true");
+//   c.status(204);
+// });
 
 const routes = app
   .basePath("/api")
