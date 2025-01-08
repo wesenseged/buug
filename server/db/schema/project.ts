4704ae68eb979d9bca56941ad1ae281488a2006f
wesenseged <wesenseged@skiff.com>
2025-01-08 17:28:29 +0300

    remove unique title

--- server/db/schema/project.ts
+++ server/db/schema/project.ts
@@ -3,7 +3,7 @@ import { serial, text, pgTable, jsonb } from "drizzle-orm/pg-core";
 export const projects = pgTable("projects", {
   id: serial("id").primaryKey(),
   userId: text("user_id").notNull(),
-  title: text("title").unique().notNull(),
+  title: text("title").notNull(),
   description: text("description").notNull(),
   status: text("status").notNull(),
   tasks: jsonb("tasks").default([]),
